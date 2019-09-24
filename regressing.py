3e9de52dfc0695c66bb8b51792c4ed468b71b1f5
denglx6 <55527308+denglx6@users.noreply.github.com>
2019-09-24 13:19:22 +0800

    Update regressing.py

--- regressing.py
+++ regressing.py
@@ -2,10 +2,8 @@
 import numpy as np
 import  math
 import matplotlib.pyplot as plt
-# import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
-num_of_point = 15
+num_of_point = 5
 x_data = np.linspace(0, 1, num_of_point, dtype=np.float32)#生成0到1指定个x坐标
 noise = np.random.normal(0, 0.1, x_data.shape).astype(np.float32)#添加均值为0方差为0.1的同维高斯噪声
 y_data = np.sin(2 * math.pi * x_data) + noise#生成相应的纵坐标带噪声数据
@@ -18,7 +16,7 @@ def regressing(x, y, m, break_err=0.2, max_iteration=20000, alpha=0.05):
         sum = np.zeros(x.shape)
         for i in range(len(x)):
             for j in range(m):
-                sum[i] += w[j] * x[i] ** j
+                sum[i] += w[j] * (x[i] ** j)
         return sum
 
     #x表示输入点横坐标向量；y表示输入点纵坐标带噪声向量；break_err表示循环终止误差；max_iteration表示最大迭代数；m表示多项式最高次数加一，即m+1次多项式系数的个数；alpha表示梯度下降学习速率；
@@ -27,6 +25,8 @@ def regressing(x, y, m, break_err=0.2, max_iteration=20000, alpha=0.05):
     for i in range(max_iteration):
         predict = f(w, x_data, m)
         err = 0.5 * np.sum(np.square(predict - y_data))
+        if i % 500 == 0:
+            print(err)
         if err <= break_err:
             print(i)
             break
@@ -77,7 +77,7 @@ def regressing_normalized(x, y, m, lamda, break_err=0.2, max_iteration=20000, al
     #绘图展示
     new_x = np.linspace(0, 1, 100, dtype=np.float32)
     plt.plot(new_x, np.sin(2 * math.pi * new_x), 'g-', label='Actual line')
-    plt.scatter(x_data, y_data, facecolors='none', edgecolors='b')
+    plt.scatter(x, y, facecolors='none', edgecolors='b')
     plt.plot(new_x, f(w, new_x, m), 'r-', label='Regressing line')
     plt.legend(loc='best')
     plt.xlabel('x')
@@ -95,6 +95,18 @@ def regressing_normalized(x, y, m, lamda, break_err=0.2, max_iteration=20000, al
 # regressing(x_data, y_data, 10)
 # plt.title('9次多项式回归示意图')
 # plt.show()   
-regressing_normalized(x_data, y_data, 10, 1e-18, break_err=0.01, max_iteration=500000, alpha=0.05)
-plt.title('9次多项式回归示意图')
+# regressing(x_data, y_data, 10, break_err=0.55, max_iteration=100000, alpha=0.01)
+# plt.title('9次多项式100点回归示意图')
+# plt.show()
+regressing_normalized(x_data, y_data, 15, 0, break_err=0.0, max_iteration=100000, alpha=0.05)
+plt.title('14次多项式正则化回归')
+plt.text(0.8, 0.5, "lnλ=-∞", size = 15, alpha = 1)
+plt.show()
+regressing_normalized(x_data, y_data, 15, 1e-18, break_err=0.05, max_iteration=50000, alpha=0.05)
+plt.title('14次多项式正则化回归')
+plt.text(0.8, 0.5, "lnλ=-18", size = 15, alpha = 1)
+plt.show()
+regressing_normalized(x_data, y_data, 15, 1, break_err=0.05, max_iteration=10000, alpha=0.05)
+plt.title('14次多项式正则化回归')
+plt.text(0.8, 0.5, "lnλ=0", size = 15, alpha = 1)
 plt.show()
